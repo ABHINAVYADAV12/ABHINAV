0cd64001c9eae6e3efd472126f2fa68042762d59
ABHINAVYADAV12 <ay747316@gamail.com>
2024-11-19 10:44:05 +0530

    complete

--- react/myproject/src/App.jsx
+++ react/myproject/src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import Student from "./student";
+import logo from "./image/abc.jpg"
 const App = () => {
   let a=20;
   let mystyle={
     backgroundColor:"cyan",
     color:"red"
   }
-  return <div style={{color:"red"}}>
+  return (
+  <div style={{color:"red"}}>
     <h2>ABES ENGINEERING COLLEGE</h2>
-  </div>;
+  <Student pic={<img src={logo} width="50px" height="50px" ></img>} name="ABHINAV YADAV" branch="CSE AIML" college="ABES ENGINEERING COLLEGE" roll="6" section="B" />
+  </div>
+  )
 };
 
 export default App;
