53ccd774ca04ec941aebffcbc08db4a0abab14c1
ABHINAVYADAV12 <ay747316@gamail.com>
2024-12-04 11:39:29 +0530

    image manipulation

--- react/myproject/src/App.jsx
+++ react/myproject/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Student from "./student";
 import logo from "./image/abc.jpg"
+import ImageManipulation from "./imageunderstand";
 const App = () => {
   let a=20;
   let mystyle={
@@ -11,6 +12,7 @@ const App = () => {
   <div style={{color:"red"}}>
     <h2>ABES ENGINEERING COLLEGE</h2>
   <Student pic={<img src={logo} width="50px" height="50px" ></img>} name="ABHINAV YADAV" branch="CSE AIML" college="ABES ENGINEERING COLLEGE" roll="6" section="B" />
+  <ImageManipulation/>
   </div>
   )
 };
