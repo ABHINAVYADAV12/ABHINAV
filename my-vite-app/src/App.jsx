6ef8c1e56e2ea0eaeea9712ee08f02ae0fde95fc
ABHINAVYADAV12 <ay747316@gamail.com>
2025-02-03 09:55:17 +0530

    abc

---
+++ my-vite-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Employer } from './employer'
+
+function App() {
+  return (
+   <div>
+    <Employer name={"Abhinav"} id={"16347"}/>
+   </div>
+  )
+}
+
+export default App
---
+++ my-vite-app/src/employer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Employer = ({name,id}) => {
+  return (
+    <div>
+    <h2>Employer Details:</h2>
+      <p>Name: {name}</p>
+      <p>ID: {id}</p>
+      </div>
+  )
+}
+
